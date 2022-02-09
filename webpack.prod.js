8b361fef026ab7a1937748ecbaf28fc94523d83e
Alexandru Leorda <leorda@realisti.co>
2022-02-09 09:21:48 +0100

    more enviorments

---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const Dotenv = require("dotenv-webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(common, {
+  output: {
+    publicPath: process.env.PROJECT_NAME
+  },
+  plugins: [
+    new Dotenv({
+      path: "./.env.production",
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })]
+  },
+});
